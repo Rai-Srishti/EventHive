331397d7962a6c3784841f1da23736f62bfde9c5
Vansh <vanshmehra2101@gmail.com>
2025-07-21 16:24:17 +0530

    test

---
+++ src/Test1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test1() {
+  return (
+    <div>
+      helloo
+    </div>
+  )
+}
+
+export default Test1
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import Test1 from './Test1.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<Test1/>
   </StrictMode>,
 )
