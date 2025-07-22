d9ca0f28ad00d8d77185e80a32158ba2a4927278
Vansh <vanshmehra2101@gmail.com>
2025-07-22 19:38:22 +0530

    Add header and hero section

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './components/Header'
+import App from './App'
 
-import Test1 from './Test1.jsx'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<Test1/>
+<App/>
   </StrictMode>,
 )
