b1de1e5ead68b68e5347722f25ce284bba5b4a0f
Vansh <vanshmehra2101@gmail.com>
2025-07-26 18:12:14 +0530

    Added EventDto And EventService

--- Event-Hive_Backend/src/main/java/com/eventhive/controllers/attendee/AttendeeController.java
+++ Event-Hive_Backend/src/main/java/com/eventhive/controllers/attendee/AttendeeController.java
@@ -1,9 +1,13 @@
 package com.eventhive.controllers.attendee;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.eventhive.dto.event.EventDto;
 import com.eventhive.services.Attendee.AttendeeService;
 
 import lombok.AllArgsConstructor;
@@ -14,4 +18,9 @@ import lombok.AllArgsConstructor;
 public class AttendeeController {
 
 	private final AttendeeService attendeeService;
+	
+	@GetMapping()
+	public ResponseEntity<List<EventDto>> getAllEvents(){
+		return null;
+	}
 }
