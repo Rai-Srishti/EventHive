afe1abe4d3d7d53e1a3d5a9913b773a9c8f538c5
amit286 <amitjamra286@gmail@.com>
2025-07-27 15:06:29 +0530

    Added Login and Register API , also added Spring Security Crypto dependency in pom.xml

---
+++ Event-Hive_Backend/src/main/java/com/eventhive/dto/authentication/LoginResponseDto.java
@@ -0,0 +1,19 @@
+package com.eventhive.dto.authentication;
+
+import com.eventhive.entities.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private Long userId;
+    private String fullName;
+    private Role role;
+    private String message;
+}
