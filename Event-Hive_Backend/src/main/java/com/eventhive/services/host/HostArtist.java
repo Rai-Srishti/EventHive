a82be99fb94de39441f7178e936fecc978c1f3c6
amit286 <amitjamra286@gmail@.com>
2025-08-01 13:48:31 +0530

    Added Get All Artist API

---
+++ Event-Hive_Backend/src/main/java/com/eventhive/services/host/HostArtist.java
@@ -0,0 +1,10 @@
+package com.eventhive.services.host;
+
+import java.util.List;
+
+import com.eventhive.dto.host.ArtistDto;
+
+public interface HostArtist {
+	public List<ArtistDto> getAllArtistNames();
+
+}
