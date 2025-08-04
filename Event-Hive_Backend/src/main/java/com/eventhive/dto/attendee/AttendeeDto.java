9b9e4f76742d08236c1b89e8b8bae704c25379ef
Srishti Rai <srishtirai2002@gmail.com>
2025-08-05 02:03:19 +0530

    Artist Swiper now fetching data from backend

--- Event-Hive_Backend/src/main/java/com/eventhive/dto/attendee/AttendeeDto.java
+++ Event-Hive_Backend/src/main/java/com/eventhive/dto/attendee/AttendeeDto.java
@@ -1,21 +1,5 @@
 package com.eventhive.dto.attendee;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.eventhive.entities.Event;
-import com.eventhive.entities.Ticket;
-import com.eventhive.entities.Wallet;
-import com.eventhive.entities.enums.Role;
-import com.eventhive.entities.enums.UserStatus;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
